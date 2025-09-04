8273b47a618b5c95183dfd95442578746fe49a05
nurealazmie002 <nurealazmie002@gmail.com>
2025-09-04 21:46:41 +0700

    tambah eval & latihan

---
+++ Bulan2/pekan1/day4/eval_latihan/eval/style.js
@@ -0,0 +1,17 @@
+/* Buat fungsi `konversiSuhu(celcius)` yang mengembalikan nilai suhu dalam 
+Fahrenheit. 
+Rumus: 
+
+`F = (C × 9/5) + 32` */
+
+
+/* function konversiSuhu(celcius) {
+  let fahrenheit = (celcius * 9/5) + 32;
+  return fahrenheit;
+}
+
+
+console.log(konversiSuhu(1));   
+console.log(konversiSuhu(50));  
+console.log(konversiSuhu(23)); 
+ */
---
+++ Bulan2/pekan1/day4/eval_latihan/latihan/style.js
@@ -0,0 +1,32 @@
+// 1. Buat fungsi `luasPersegi(sisi)` yang mengembalikan hasil sisi × sisi 
+
+
+
+/* function luasPersegi(sisi) {
+    return sisi * sisi
+}       
+console.log(luasPersegi(5)) */
+
+
+
+
+
+// 2. Buat fungsi untuk menghitung jumlah 3 angka 
+
+/* function jumlahTigaAngka(angka1, angka2, angka3) {
+return angka1 + angka2 + angka3
+}
+let jumlah = jumlahTigaAngka(28, 2, 45)
+console.log(jumlah) */
+
+
+
+
+
+
+// 3. Buat fungsi `isEven(angka)` yang mengembalikan true jika angka tersebut genap, false jika ganjil
+
+/* function isEven(angka) {
+    return angka % 2 === 0
+}
+console.log(isEven(2)) */
