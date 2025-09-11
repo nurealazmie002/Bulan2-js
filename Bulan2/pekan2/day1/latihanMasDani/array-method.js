202b63a394a94a2d5c3a305e92f80b41035016a9
nurealazmie002 <nurealazmie002@gmail.com>
2025-09-11 10:06:12 +0700

    tambah day3 & pembenahan file lain

--- Bulan2/pekan2/day1/latihanMasDani/array-method.js
+++ Bulan2/pekan2/day1/latihanMasDani/array-method.js
@@ -200,7 +200,16 @@ console.log(makanan) */
 
 //latihan Main
 
+
+
 /* let angka = [1,2,3,4,5,6,7,8,9,10]
+
+
+angka.forEach(function(n) {
+  console.log("Aku punya angka: " + n);
+});
+
+
 
 let kaliLima = angka.map(function(n){
     return n * 5
